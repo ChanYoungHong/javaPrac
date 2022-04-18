67a76d240ae0cafba7502dea29f70a279f1f1d51
ChanYoungHong <cksdud9883@naver.com>
2022-04-18 18:49:40 +0900

    자바의 정석 공부 첫 날

---
+++ src/main/java/StandardOfJavaExercise/day1/Exercise3_4.java
@@ -0,0 +1,14 @@
+package StandardOfJavaExercise.day1;
+
+public class Exercise3_4 {
+    public static void main(String[] args) {
+
+        int num = 456;
+        int result = 56;
+
+        int hi = num - result;
+        System.out.println(hi);
+        System.out.println();
+    }
+}
+
